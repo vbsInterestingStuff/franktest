cbefdbe8cea3742af3e8e714adfd943d9b9d75cc
Vlad Babii <vladbabii@users.noreply.github.com>
2018-12-12 07:35:24 +0200

    Update api.js

--- fastify/api.js
+++ fastify/api.js
@@ -1,1 +1,13 @@
+const fastify = require('fastify')({
+  logger: true
+})
 
+fastify.get('/', async (request, reply) => {
+  reply.type('application/json').code(200)
+  return { hello: 'world' }
+})
+
+fastify.listen(80, (err, address) => {
+  if (err) throw err
+  fastify.log.info(`server listening on ${address}`)
+})
